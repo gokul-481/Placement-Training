f5636b9b95b81f13b1090af5d6630ba09599a09b
Gokul <gokulamurugan7@gmail.com>
2025-02-12 15:58:13 +0530

    Add files via upload

---
+++ Prime Number.java
@@ -0,0 +1,32 @@
+class Main
+{
+    public static void main(String[] args)
+    {
+        int n = 3;
+        int count = 0;
+        if (n <= 1)
+        {
+            System.out.println("The number is not prime");
+            return;
+        }
+        for (int j = 1; j <= n / 2; j++)
+        {
+            if (n % j == 0)
+            {
+                count++;
+                
+            }
+        }
+        if (count > 1)
+        {
+            System.out.println("The number is not prime");
+        }
+    else {
+        System.out.println("The number is prime");
+         }
+        
+    }
+}
+
+
+    
