c9c96e51084a04f9ea3bb8d596e01a1c62b8c4d3
Gokul <gokulamurugan7@gmail.com>
2025-02-12 14:47:24 +0530

    Add files via upload
    SHORT CIRCUIT OPERATOR

--- Main.java
+++ Main.java
@@ -2,6 +2,11 @@ class Main
 {
 	public static void main(String s[])
 	{
-		System.out.println("Hello World");
+		int a=50;
+		int b=100;
+		int c=150;
+		System.out.println("USING SHORT CIRCUIT OPERATOR:");
+		System.out.println((a>b) && (a<b));
+		System.out.println((a>b) || (a<b));
 	}
 }
