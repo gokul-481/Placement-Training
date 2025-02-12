fe52a359abee00e569978e26abb6371d626402ce
Gokul <gokulamurugan7@gmail.com>
2025-02-12 12:00:34 +0530

    Add files via upload

---
+++ NON-SHORT CIRCUIT OPERATOR.java
@@ -0,0 +1,12 @@
+class Main
+{
+	public static void main(String s[])
+	{
+		int a=50;
+		int b=100;
+		int c=150;
+		System.out.println("USING NON-SHORT CIRCUIT OPERATOR:");
+		System.out.println((a>b) & (a<b));
+		System.out.println((a>b) | (a<b));
+	}
+}
