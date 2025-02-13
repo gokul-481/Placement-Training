e766297ec620fdd3dace9631ea0970b176734eee
Gokul <gokulamurugan7@gmail.com>
2025-02-13 12:01:51 +0530

    Add files via upload

---
+++ Nested Loop.java.java
@@ -0,0 +1,12 @@
+public class Main
+{
+	public static void main(String[] args) {
+		for(i=1;i<=n;i++)
+		{
+			for(j=1;j<=i;j++)
+			{
+				System.out.println(1);
+			}System.out.println();
+		}
+} 
+}
