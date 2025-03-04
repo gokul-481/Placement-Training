93220f6f0a2e6402401eaf649165bac171f9d44b
Gokul <gokulamurugan7@gmail.com>
2025-03-04 16:13:52 +0530

    Delete 04-03-2025/SubStr_StrBuilderObj.java

--- 04-03-2025/SubStr_StrBuilderObj.java
+++
@@ -1,8 +0,0 @@
-class SubStr_StrBuilderObj{
-    public static void main (String[] args) {
-        String s="NAVAN";
-        StringBuilder s1=new StringBuilder(s);
-        String sub=s1.substring(1,4);
-        System.out.println(sub);
-    }
-}
