bef043b147e3fcc203a96edecac373655caffe1a
Gokul <gokulamurugan7@gmail.com>
2025-03-04 16:14:12 +0530

    Delete 04-03-2025/SubString_StringObj.java

--- 04-03-2025/SubString_StringObj.java
+++
@@ -1,7 +0,0 @@
-class SubString_StringObj{
-    public static void main (String[] args) {
-        String s="NAVAN";
-        String sub=s.substring(1,4);
-        System.out.println(sub);
-    }
-}
